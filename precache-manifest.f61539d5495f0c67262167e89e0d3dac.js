90d41f1c019b2272c6c0fe0ecd7f8d62c64bce2b
Bratislav Stojanovic <bata.stojan.83@gmail.com>
2019-08-11 23:15:25 +0200

    Updates

---
+++ precache-manifest.f61539d5495f0c67262167e89e0d3dac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda3a11c2a0fce13050e6b22f64d5098",
+    "url": "/githubFinder/index.html"
+  },
+  {
+    "revision": "f56efb19562bb2c7bcf3",
+    "url": "/githubFinder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "15d01be9aa2676e13cdf",
+    "url": "/githubFinder/static/js/2.d8131e92.chunk.js"
+  },
+  {
+    "revision": "f56efb19562bb2c7bcf3",
+    "url": "/githubFinder/static/js/main.fcef797f.chunk.js"
+  },
+  {
+    "revision": "5cae73cc1a7202fd9926",
+    "url": "/githubFinder/static/js/runtime~main.75e1c857.js"
+  }
+]);
